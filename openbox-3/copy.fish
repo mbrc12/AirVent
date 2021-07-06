bd2bae5440032b061dfd73b82f2e5d049af56a11
Mriganka Basu Roy Chowdhury <mbrc12@gmail.com>
2021-07-06 16:17:22 +0530

    First commit

---
+++ openbox-3/copy.fish
@@ -0,0 +1,11 @@
+cp button.xbm max.xbm
+cp button.xbm max_disabled.xbm
+cp button.xbm max_toggled.xbm
+cp button.xbm iconify.xbm
+cp button.xbm close.xbm
+cp button.xbm max.xbm
+cp button.xbm desk.xbm
+cp button.xbm desk_toggled.xbm
+
+
+
